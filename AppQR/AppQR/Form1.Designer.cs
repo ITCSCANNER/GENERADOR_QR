83e4a29e814f68bf958cc6f5a539cf37e97a10df
Marianotc <mariano_terrazas@hotmail.com>
2016-11-01 11:28:06 -0700

    Proyecto exe

---
+++ AppQR/AppQR/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AppQR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.panelResultado = new System.Windows.Forms.Panel();
+            this.btnCodificado = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(28, 231);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(200, 39);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Location = new System.Drawing.Point(28, 321);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(200, 39);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // panelResultado
+            // 
+            this.panelResultado.Location = new System.Drawing.Point(28, 12);
+            this.panelResultado.Name = "panelResultado";
+            this.panelResultado.Size = new System.Drawing.Size(200, 200);
+            this.panelResultado.TabIndex = 3;
+            // 
+            // btnCodificado
+            // 
+            this.btnCodificado.Location = new System.Drawing.Point(28, 276);
+            this.btnCodificado.Name = "btnCodificado";
+            this.btnCodificado.Size = new System.Drawing.Size(200, 39);
+            this.btnCodificado.TabIndex = 7;
+            this.btnCodificado.Text = "Codificado";
+            this.btnCodificado.UseVisualStyleBackColor = true;
+            this.btnCodificado.Click += new System.EventHandler(this.btnCodificado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 406);
+            this.Controls.Add(this.btnCodificado);
+            this.Controls.Add(this.panelResultado);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Panel panelResultado;
+        private System.Windows.Forms.Button btnCodificado;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AppQR/AppQR/Form1.cs
@@ -0,0 +1,128 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppQR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            DateTime fechaAc = DateTime.Now;
+            var fecha = fechaAc.ToString("yyyy-MM-dd|");
+            var hora = fechaAc.ToString("HH:mm");
+            var aula = "A1|";
+            var FechaCompleta = aula+fecha+"|"+hora;
+
+            
+            /*var FechaCompletaCod = "";
+            for (int i=0; i< FechaCompleta.Length; i++)
+            {
+                var letra = Encoding.ASCII.GetBytes(FechaCompleta)[i];
+                letra += 6;
+                FechaCompletaCod += Convert.ToChar(letra).ToString();
+            }
+
+            var FechaCompletaDec = "";
+            for (int i = 0; i < FechaCompletaCod.Length; i++)
+            {
+                var letra = Encoding.ASCII.GetBytes(FechaCompletaCod)[i];
+                letra -= 6;
+                FechaCompletaDec += Convert.ToChar(letra).ToString();
+            }
+            MessageBox.Show(FechaCompletaCod+ "\n" + FechaCompletaDec, "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            */
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(FechaCompleta, out qrCode);
+
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black,Brushes.White);
+
+            MemoryStream ms = new MemoryStream();
+
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var imagenTemporal = new Bitmap(ms);
+            var imagen = new Bitmap(imagenTemporal, new Size(new Point(200, 200)));
+            panelResultado.BackgroundImage = imagen;
+
+            btnGuardar.Enabled = true;
+        }
+
+        private void btnCodificado_Click(object sender, EventArgs e)
+        {
+            
+            DateTime fechaAc = DateTime.Now;
+            var fecha = fechaAc.ToString("yyyy - MM - dd");
+            var hora = fechaAc.ToString("HH:mm");
+            var aula = "A1 ";
+            var FechaCompleta = aula + fecha + " " + hora;
+
+
+            var FechaCompletaCod = "";
+            for (int i = 0; i < FechaCompleta.Length; i++)
+            {
+                var letra = Encoding.ASCII.GetBytes(FechaCompleta)[i];
+                letra += 6;
+                FechaCompletaCod += Convert.ToChar(letra).ToString();
+            }
+
+            var FechaCompletaDec = "";
+            for (int i = 0; i < FechaCompletaCod.Length; i++)
+            {
+                var letra = Encoding.ASCII.GetBytes(FechaCompletaCod)[i];
+                letra -= 6;
+                FechaCompletaDec += Convert.ToChar(letra).ToString();
+            }
+            //MessageBox.Show(FechaCompletaCod + "\n" + FechaCompletaDec, "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            
+
+
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(FechaCompletaCod, out qrCode);
+
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+
+            MemoryStream ms = new MemoryStream();
+
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var imagenTemporal = new Bitmap(ms);
+            var imagen = new Bitmap(imagenTemporal, new Size(new Point(200, 200)));
+            panelResultado.BackgroundImage = imagen;
+
+            btnGuardar.Enabled = true;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Image imgFinal = (Image)panelResultado.BackgroundImage.Clone();
+
+            SaveFileDialog cajaDeDialogoGuardar = new SaveFileDialog();
+            cajaDeDialogoGuardar.AddExtension = true;
+            cajaDeDialogoGuardar.Filter = "Image PNG (*.png) | *.png";
+            cajaDeDialogoGuardar.ShowDialog();
+            if (!string.IsNullOrEmpty(cajaDeDialogoGuardar.FileName))
+            {
+                imgFinal.Save(cajaDeDialogoGuardar.FileName, ImageFormat.Png);
+            }
+            imgFinal.Dispose();
+        }
+    }
+}
